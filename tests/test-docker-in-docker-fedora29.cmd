92b552e8acdb9dce9b3f89d1db253ce6cdf81393
Kimmo Lehto <klehto@mirantis.com>
2023-09-20 11:25:49 +0300

    Delete tests from dropped images, enable skipped
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- tests/test-docker-in-docker-fedora29.cmd
+++
@@ -1,6 +0,0 @@
-%defer footloose delete --config %testName.yaml
-footloose create --config %testName.yaml
-footloose --config %testName.yaml ssh root@node0 -- yum install -y docker iptables
-footloose --config %testName.yaml ssh root@node0 systemctl start docker
-footloose --config %testName.yaml ssh root@node0 docker pull busybox
-%out footloose --config %testName.yaml ssh root@node0 docker run busybox echo success
--- tests/test-docker-in-docker-ubuntu16.04.cmd
+++
@@ -1,6 +0,0 @@
-%defer footloose delete --config %testName.yaml
-footloose create --config %testName.yaml
-footloose --config %testName.yaml ssh root@node0 -- apt-get update && apt-get install -y docker.io
-footloose --config %testName.yaml ssh root@node0 systemctl start docker
-footloose --config %testName.yaml ssh root@node0 docker pull busybox
-%out footloose --config %testName.yaml ssh root@node0 docker run busybox echo success
