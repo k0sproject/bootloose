0bfec6e3cc7523e30b0d1177b6f485084611ffb2
Kimmo Lehto <klehto@mirantis.com>
2023-09-21 15:13:43 +0300

    Ensure cfg does not exist before test
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- tests/test-create-config-already-exists-%image.cmd
+++ tests/test-create-config-already-exists-%image.cmd
@@ -1,5 +1,6 @@
 # Tests that footloose config create fails if the config already exists
 
+rm -f %testName.footloose
 footloose config create --config %testName.footloose --name %testName --key %testName-key --image %image
 %defer rm -f %testName.footloose %testName-key %testName-key.pub
 
